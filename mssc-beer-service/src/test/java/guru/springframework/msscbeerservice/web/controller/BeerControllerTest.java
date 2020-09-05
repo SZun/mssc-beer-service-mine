b8ac8acb726dad97aeaef150a83f6e376736ea16
Zun <zunsamuel97@gmail.com>
2020-09-05 15:26:17 -0500

    step 117

--- mssc-beer-service/src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ mssc-beer-service/src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,17 +1,21 @@
 package guru.springframework.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.msscbeerservice.services.BeerService;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,8 +28,15 @@ class BeerControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private BeerService beerService;
+
+    private final String BEER_1_UPC = "0631234200036";
+
     @Test
     void getBeerById() throws Exception {
+
+        when(beerService.getById(any(UUID.class))).thenReturn(getValidBeerDto());
 
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString())
                 .accept(MediaType.APPLICATION_JSON))
@@ -35,6 +46,8 @@ class BeerControllerTest {
 
     @Test
     void saveNewBeer() throws Exception {
+
+        when(beerService.saveNewBeer(any(BeerDto.class))).thenReturn(getValidBeerDto());
 
         String beerDtoJson = objectMapper.writeValueAsString(getValidBeerDto());
 
@@ -59,7 +72,7 @@ class BeerControllerTest {
                 .beerName("My Beer")
                 .beerStyle(BeerStyleEnum.ALE)
                 .price(new BigDecimal("2.99"))
-                .upc(1232123212321L)
+                .upc(BEER_1_UPC)
                 .build();
     }
 }
